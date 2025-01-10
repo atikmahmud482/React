1f54e3c401ab244311d76a8e27fddc82a6033b58
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-10 21:14:59 +0600

    39-2 button add

--- explore-react-2/explore-react-2/src/App.jsx
+++ explore-react-2/explore-react-2/src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("button click");
+  }
+
   return (
     <>
       <h1>Core concept two</h1>
+      {/* 1st option */}
+      <button onClick={handleClick}>Click Me</button>
+
+      {/* 2nd option */}
+      <button
+        onClick={function handleClick2() {
+          alert("You click Me");
+        }}>
+        Click Me 2
+      </button>
     </>
   );
 }
